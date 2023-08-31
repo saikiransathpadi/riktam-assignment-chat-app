7c73abda787f612f41068025e203905d27bb42e4
Sathpadi SaiKiran <sathpadi.saikiran@bijak.in>
2023-08-31 10:59:36 +0530

    message changes

--- src/enums/apiRes.ts
+++ src/enums/apiRes.ts
@@ -9,7 +9,7 @@ export enum STATUS_CODES {
 
 export enum RESPONSE_MESSAGES {
     UNAUTHORIZED = 'User is not authorized with an explicit deny.',
-    TOKEN_MISSING = 'Token is missing',
+    TOKEN_MISSING = 'Token is missing / Expired',
     SUCCESS = 'SUCCESS',
     GENERAL_MESSAGE = 'Something went wrong, Please try again',
     GENERAL_MESSAGE_CONTACT = 'Something went wrong, Please contact customer care',
